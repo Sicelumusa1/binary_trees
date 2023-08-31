e14a81cbc0293c3da2a835304d22224d57de25a6
Musa <musaqwabe@gmail.com>
2023-08-31 13:13:33 -0700

    added file 19

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - returns a pointer to the lowest 
+ * common ancestor of two nodes in a binary tree
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: pointer to the lowest common ancestor of two nodes
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, 
+		const binary_tree_t *second)
+{
+	const binary_tree_t *ancestor;
+	const binary_tree_t *current;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	ancestor = first;
+	while (ancestor != NULL)
+	{
+		current = second;
+		while (current != NULL)
+		{
+			if (ancestor == current)
+				return (binary_tree_t *)ancestor;
+			current = current->parent;
+		}
+		ancestor = ancestor->parent;
+	}
+
+	return (NULL);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -49,5 +49,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 int binary_tree_is_perfect_recursive(const binary_tree_t *tree,
         size_t height, size_t level);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, 
+		const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 
 #endif /*BINARY_TREES_H*/
